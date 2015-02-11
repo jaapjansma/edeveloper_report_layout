76ffed4275d4aab987d78257f3057a0f516aed37
Jaap Jansma <jaap@edeveloper.nl>
2015-02-11 21:58:55 +0100

    first commit

---
+++ __openerp__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Edeveloper report layout',
+    'version': '1.0',
+    'category': '',
+    "sequence": 14,
+    'complexity': "easy",
+    'category': 'Hidden',
+    'description': """
+        Edeveloper custom report layout
+    """,
+    'author': 'Jaap Jansma (Edeveloper)',
+    'website': 'www.edeveloper.nl',
+    'depends': ["base", "report"],
+    'init_xml': [],
+    'data': [
+        "layout.xml",
+        "header.xml",
+        "footer.xml",
+    ],
+    'demo_xml': [],
+    'test': [],
+    'qweb' : [],
+    'installable': True,
+    'auto_install': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
